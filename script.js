253641b717cdd40c01479bfb1e1b86e42995215c
Daniel <gabrielang56799@gmail.com>
2025-12-10 13:08:42 +0530

    addednav

---
+++ script.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('#btn');
+let mobileNav = document.querySelector('.mobile-nav');
+let menubtn = document.querySelector('#menubtn');
+let closebtn = document.querySelector('#closebtn');
+let active = false;
+
+btn.addEventListener('click', () => {
+   active?mobileNav.style.height="0":mobileNav.style.height ="auto"
+    menubtn.style.display = active?"block":"none";
+    closebtn.style.display = active?"none":"block";
+    active = !active;
+
+});
