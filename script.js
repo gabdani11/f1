fdb46137d1987a7093a62d752b87e658f8ba393e
Daniel <gabrielang56799@gmail.com>
2025-12-15 13:04:28 +0530

    fix transtion

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let closebtn = document.querySelector('#closebtn');
 let active = false;
 
 btn.addEventListener('click', () => {
-   active?mobileNav.style.height="0":mobileNav.style.height ="auto"
+   active?mobileNav.style.height="0px":mobileNav.style.height ="200px";
     menubtn.style.display = active?"block":"none";
     closebtn.style.display = active?"none":"block";
     active = !active;
